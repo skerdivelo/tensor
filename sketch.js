2325ad0e1ea6deb90b458ad2080ae2bfcac2648f
skerdivelo <skerdivelo42@gmail.com>
2023-11-01 16:05:57 +0100

    Usato vettore

--- sketch.js
+++ sketch.js
@@ -1,5 +1,12 @@
 function setup() {
-    const data = tf.tensor([0, 0, 127,45324,345,453,5334,35], [2, 2, 2]); //creo un tensor che ha 2 matrici da 2x2 (2x2x2=8 numeri)
+
+    const values = [];
+    for(let i = 0; i < 15; i++){
+        values[i] = random(0, 100);
+    }
+    const shape = [5, 3];
+    const data = tf.tensor(values, shape);
+    //const data = tf.tensor([0, 0, 127,45324,345,453,5334,35,34,345,23,456,345,456,345], [5,3]); //creo un tensor che ha 2 matrici da 2x2 (2x2x2=8 numeri)
     data.print();
 }
 setup();
